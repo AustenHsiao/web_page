9ab2e30fde0f02a2d26b11938673fe17b49e7220
austenhsiao <austenhsiao@gmail.com>
2021-11-20 05:30:50 -0800

    added ability to add scrolling element

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 var deck = document.getElementsByClassName("card");
+var image_index = 0;
+
+for (var i = 0; i < deck.length; ++i) {
+    deck[i].setAttribute("style", "display: none;")
+}
+
+deck[image_index].setAttribute("style", "display:inline-block; max-width: 30vw;");
+
+function cycle_left() {
+    deck[image_index].setAttribute("style", "display:none; max-width: 30vw;");
+    if (--image_index < 0) {
+        image_index = deck.length - 1;
+    }
+    deck[image_index].setAttribute("style", "display:inline-block; max-width: 30vw;");
+}
+
+function cycle_right() {
+    deck[image_index].setAttribute("style", "display:none; max-width: 30vw;");
+    if (++image_index >= deck.length) {
+        image_index = 0;
+    }
+    deck[image_index].setAttribute("style", "display:inline-block; max-width: 30vw;");
+}
